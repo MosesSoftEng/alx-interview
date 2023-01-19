fba4a1254879d44c62ac12ff9c795a1a610f22f0
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-19 19:26:33 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,34 +4,28 @@
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+# Initialize variables to store metrics
 total_size = 0
-counter = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
-
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+# Read input line by line
+for line in sys.stdin:
+    # Check if line matches the specified format
+    parts = line.split()
+    if len(parts) != 6:
+        continue
+    ip, date, request, status, code, size = parts
+    status_code = int(code)
+    # Check if status code is in the list of possible codes
+    if status_code not in status_codes:
+        continue
+    # Update metrics
+    total_size += int(size)
+    status_codes[status_code] += 1
+    line_count += 1
+    # Print metrics every 10 lines
+    if line_count % 10 == 0:
+        print("Total file size: ", total_size)
+        for code in sorted(status_codes):
+            print(f"{code}: {status_codes[code]}")
