ee186304b97eaae9e8910300bbe5bbe4b4347259
MosesSoftEng <moses.soft.eng@gmail.com>
2023-02-09 14:35:49 +0300

    Add solution for starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
+
+starwarsCharacters(filmID);
--- 0x06-starwars_api/README.md
+++ 0x06-starwars_api/README.md
@@ -2,10 +2,13 @@
 
 This project contains interview coding challenges.
 
-## Tasks To Complete
+```bash
+touch 0-starwars_characters.js
+chmod +x 0-starwars_characters.js
 
-+ [x] 0. **Star Wars Characters**<br/>[0-starwars_characters.js](0-starwars_characters.js) contains a script that prints all characters of a Star Wars movie with the following requirements:
-  + The first positional argument passed is the Movie ID - example: `3` = “Return of the Jedi”.
-  + Display one character name per line **in the same order as the “characters” list in the `/films/` endpoint**.
-  + You must use the [Star wars API](https://swapi-api.hbtn.io/).
-  + You must use the `request` module.
+# Lint.
+pycodestyle 0-starwars_characters.js
+
+# tests.
+./0-generator.py | ./0-stats.py 
+``
