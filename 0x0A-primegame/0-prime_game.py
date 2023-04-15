60e223b57ccc12dd9dd35f61aa0935fcf1c87ad3
Moses Mwangi <moses.soft.eng@gmail.com>
2023-04-15 18:28:34 +0300

    Add solution for task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,44 +3,55 @@
 
 
 def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds.
-    """
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    """Function to get who has won in prime game"""
+    mariaWinsCount = 0
+    benWinsCount = 0
 
-    winners = {'Maria': 0, 'Ben': 0}
-    for n in nums:
-        if max(nums) < 2:
-            winners['Ben'] += 1
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
+
+        if not primesSet:
+            benWinsCount += 1
             continue
 
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        maria_turn = True
-        while primes:
-            can_remove = False
-            for p in primes:
-                if n % p == 0:
-                    can_remove = True
-                    primes.remove(p)
-                    for i in range(p, n + 1, p):
-                        if i in primes:
-                            primes.remove(i)
-                    n -= n // p
-                    break
-            if not can_remove:
-                break
-            maria_turn = not maria_turn
-        if maria_turn:
-            winners['Ben'] += 1
-        else:
-            winners['Maria'] += 1
+        isMariaTurns = True
 
-    if winners['Maria'] == winners['Ben']:
-        return None
-    else:
-        return max(winners, key=winners.get)
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
+
+
+def is_prime(n):
+    """Returns True if n is prime, else False."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    """Returns a list of prime numbers between start and end (inclusive)."""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
--- 0x0A-primegame/README.md
+++ 0x0A-primegame/README.md
@@ -4,7 +4,7 @@ Prime game interview question.
 
 # :computer: Tasks.
 <!---->
-## [0. Island Perimeter](0-prime_game.py)
+## [0. Prime Game](0-prime_game.py)
 ### :page_with_curl: Task requirements.
 Maria and Ben are playing a game. Given a set of consecutive integers starting from `1` up to and including `n`, they take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game.
 
